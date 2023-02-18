00940e9c08516400a38a20be4651463b614c0039
MasubaCoder10 <124436859+MasubaCoder10@users.noreply.github.com>
2023-02-18 15:04:18 +0100

    Add files via upload

---
+++ Js04/checkEvenOdd.js
@@ -0,0 +1,13 @@
+function pairImpair(x) {
+    if (x % 2 != 0) {
+        return console.log("Impair")
+    } else {
+        return console.log("Pair");
+    }
+}
+
+pairImpair(15);
+pairImpair(10);
+
+
+//module.exports = checkEvenOdd
---
+++ Js04/multiply.js
@@ -0,0 +1,7 @@
+function product(a, b) {
+    return a * b;
+}
+
+console.log(product(10, 5));
+
+//module.exports = multiply
